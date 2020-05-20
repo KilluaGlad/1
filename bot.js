d72d470e9bba3410f001778563d49f577df6d8a5
KilluaGlad <45149529+KilluaGlad@users.noreply.github.com>
2020-05-20 18:03:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
 ﻿const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
 
 client.on('ready', () => {
    console.log(`----------------`);
@@ -16,48 +19,78 @@ client.on('ready', () => {
 
 
 client.on('message', message => {
-    if(message.content === '-daily'){
+    if(message.content === '$d'){
         message.channel.send('#daily')
     }
 });
 
 client.on('message', message => {
-    if(message.content === '-credits'){
+    if(message.content === '$c'){
         message.channel.send('#credits')
     }
-});
-
-client.on('message', message => {
-    if(message.content === '-ريب'){
-        message.channel.send('#credits')
-    }
+   if(message.content.startsWith("$s")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+   }
 });
 
 client2.on('message', message => {
-    if(message.content === '-daily'){
-        message.channel.send('#daily')
-    }
+  if(message.content === '$d'){
+ message.channel.send('#daily')
+  }
+  if(message.content === '$c'){
+ message.channel.send('#credits')
+  }
+  if(message.content.startsWith("$s")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
 });
 
-client2.on('message', message => {
-    if(message.content === '-credits'){
-        message.channel.send('#credits')
-    }
+client3.on('message', message => {
+  if(message.content === '$d'){
+ message.channel.send('#daily')
+  }
+  if(message.content === '$c'){
+ message.channel.send('#credits')
+  }
+  if(message.content.startsWith("$s")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
 });
 
-client2.on('message', message => {
-    if(message.content === '-ريب'){
-        message.channel.send('#credits')
-    }
+client4.on('message', message => {
+  if (message.content === '$d') {
+ message.channel.send('#daily')
+  }
+  if(message.content === '$c'){
+ message.channel.send('#credits')
+  }
+  if(message.content.startsWith("$s")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
 });
 
-
+client5.on('message', message => {
+  if(message.content === '$d'){
+message.channel.send('#daily')
+  }
+  if(message.content === '$c'){
+message.channel.send('#credits')
+  }
+  if(message.content.startsWith("$s")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+});
 
 client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam1') {
+if (message.content === '$spam') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
+      for(let x = 0; x < 9000; x++) {
         message.channel.send(`**Credit Spam , Frame , Credit By Killua , Farm Credit By killua, ez Farm - dix vitasse tjm3 credits 7ta t3ya **[ " ${x} " ]`)
           .then(m => {
             count++;
@@ -68,10 +101,52 @@ if (message.content === '!spam1') {
 });
 
 client2.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam1') {
+if (message.content === '$spam') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
+      for(let x = 0; x < 9000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Killua , Farm Credit By killua, ez Farm - dix vitasse tjm3 credits 7ta t3ya **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client3.on('message', message => {
+if (message.content === '$spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 9000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Killua , Farm Credit By killua, ez Farm - dix vitasse tjm3 credits 7ta t3ya **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client4.on('message', message => {
+if (message.content === '$spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 9000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Killua , Farm Credit By killua, ez Farm - dix vitasse tjm3 credits 7ta t3ya **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client5.on('message', message => {
+if (message.content === '$spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 9000; x++) {
         message.channel.send(`**Credit Spam , Frame , Credit By Killua , Farm Credit By killua, ez Farm - dix vitasse tjm3 credits 7ta t3ya **[ " ${x} " ]`)
           .then(m => {
             count++;
@@ -83,6 +158,8 @@ if (message.content === '!spam1') {
 
 
 
-
 client.login(process.env.TOKEN);// لا تغير فيها شيء
 client2.login(process.env.TOKEN2);// لا تغير فيها شيء
+client3.login(process.env.TOKEN3);
+client4.login(process.env.TOKEN4);
+client5.login(process.env.TOKEN5);
